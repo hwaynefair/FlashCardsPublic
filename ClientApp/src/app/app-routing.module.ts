0c8bfa637e6f438aaedb8d6a8166dcebdd7c889f
Wayne Fair <hwaynefair@yahoo.com>
2019-01-07 16:18:45 -0600

    routing simplified

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AddCardComponent} from './add-card/add-card.component'
 import { DocardsComponent } from './docards/docards.component';
 
-const routes: Routes = [{path: '', component: DocardsComponent},{ path: 'doCards', component: DocardsComponent }, {path: 'addCard', component: AddCardComponent} ];
+//default is set to DocardsComponent
+const routes: Routes = [{path: '', component: DocardsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/src/app/qa.ts
+++ ClientApp/src/app/qa.ts
@@ -4,7 +4,7 @@ export class QA {
         public id:number=0,
         public question: string='',
         public  answer: string='',
-        public  category: string='',
+        public  category: string='Angular',
         public answered:boolean=false
       ) {  }
 }
