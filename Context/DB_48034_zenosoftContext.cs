f2df8d6044cdd2b10a59dfa5b7a1e8dfa595e9e6
Wayne Fair <hwaynefair@yahoo.com>
2019-01-07 14:28:13 -0600

    cleanup security

--- Context/DB_48034_zenosoftContext.cs
+++ Context/DB_48034_zenosoftContext.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace CoreAPI21withAngular.Models
+namespace CoreAPI21
 {
     public partial class DB_48034_zenosoftContext : DbContext
     {
@@ -14,19 +14,32 @@ namespace CoreAPI21withAngular.Models
             : base(options)
         {
         }
+        public virtual DbSet<FlashCards> FlashCards { get; set; }       
+        // Unable to generate entity type for table 'dbo.BUActs'. Please see the warning messages.
+        // Unable to generate entity type for table 'dbo.PaletteColorBU'. Please see the warning messages.
+        // Unable to generate entity type for table 'dbo.Websites'. Please see the warning messages.
 
-        // Unable to generate entity type for table 'dbo.FlashCards'. Please see the warning messages.
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=s18.winhost.com;Database=DB_48034_zenosoft;User Id=DB_48034_zenosoft_user;Password=innerfit7;");
-            }
-        }
+  
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {}
+        {
+            modelBuilder.Entity<FlashCards>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Answer).HasColumnName("answer");
+
+                entity.Property(e => e.Answered).HasColumnName("answered");
+
+                entity.Property(e => e.Category)
+                    .HasColumnName("category")
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.Question).HasColumnName("question");
+            });
+
+
+          
+        }
     }
 }
